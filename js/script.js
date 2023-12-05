c0e9922d52e9b3cce073c7d85d96613a9a17aa2d
Coletti <matheus.coletti08@gmail.com>
2023-12-05 10:27:49 -0300

    update resources

--- js/script.js
+++ js/script.js
@@ -11,25 +11,22 @@ function toggleText() {
   }
 }
 
-// Senhas corretas para o login (substitua com as senhas desejadas)
+
 let senhaCorreta = 'amor';
 
-// Função para validar o login
 document.getElementById('loginForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Evita o envio automático do formulário
 
   let password = document.getElementById('password').value;
 
-  // Verifica se a senha fornecida corresponde à senha correta
   if (password === senhaCorreta) {
-      document.getElementById('loginSection').style.display = 'none'; // Esconde a seção de login
-      document.getElementById('hiddenText').style.display = 'block'; // Mostra o conteúdo após o login bem-sucedido
+      document.getElementById('loginSection').style.display = 'none'; 
+      document.getElementById('hiddenText').style.display = 'block';
   } else {
       document.getElementById('loginMessage').innerText = 'Senha incorreta. Tente novamente.';
   }
 });
 
-// Adiciona manipulador de eventos de toque (touchstart) para o campo de senha
 document.getElementById('password').addEventListener('touchstart', function(event) {
-  event.preventDefault(); // Evita o comportamento padrão do evento de toque
+  event.preventDefault();
 });
