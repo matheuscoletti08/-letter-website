6d28c8bf472c16ce3ef4d635acb0b503da9b3345
Coletti <matheus.coletti08@gmail.com>
2023-12-05 00:14:59 -0300

    update js

--- js/script.js
+++ js/script.js
@@ -17,14 +17,21 @@ const senhasCorretas = ['amor', 'coletti', 'nataly'];
 // Função para validar o login
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita o envio do formulário
-
     const password = document.getElementById('password').value;
+    validaSenha(password);
+});
 
-    // Verifica se a senha fornecida corresponde a alguma das senhas corretas na lista
+document.getElementById('loginForm').addEventListener('touchstart', function(event) {
+    event.preventDefault(); // Evita o evento de toque padrão
+    const password = document.getElementById('password').value;
+    validaSenha(password);
+});
+
+function validaSenha(password) {
     if (senhasCorretas.includes(password)) {
         document.getElementById('loginSection').style.display = 'none'; // Esconde a seção de login
         document.getElementById('hiddenText').style.display = 'block'; // Mostra o conteúdo após o login bem-sucedido
     } else {
         document.getElementById('loginMessage').innerText = 'Senha incorreta. Tente novamente.';
     }
-});
+}
